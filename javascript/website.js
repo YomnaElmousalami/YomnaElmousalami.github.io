acca38878ffec22b640292602646e2b3422f31b8
YomnaElmousalami <yomnaelmousalami@gmail.com>
2025-07-27 18:59:54 -0400

    update resume

--- javascript/website.js
+++ javascript/website.js
@@ -1,6 +1,6 @@
 function downloadResume() {
     const resume = document.createElement("a");
-    resume.href = "https://drive.google.com/uc?export=download&id=14GX06n2trqafWFuqP9_wpr9O6ZvcXwb6";
+    resume.href = "https://drive.google.com/uc?export=download&id=1sUAKxILq3yeILiP2yBGeNcYbje8TjiW3";
     resume.download = "Yomna_Elmousalami_Resume.pdf";
     document.body.appendChild(resume);
     resume.click();
